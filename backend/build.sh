171f502c85f88291d404c79e82e404f6517300b2
syxn0 <yannix2004@gmail.com>
2025-05-23 04:27:28 +0800

    Minor fixes

---
+++ backend/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -o errexit
+
+# Install dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
+
+# Run migrations
+python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput 
